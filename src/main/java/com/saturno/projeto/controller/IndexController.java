0f36780a7a0680e48995e71e1f6817eb1e9530f3
alissonpeixer <alissonpeixer4@gmail.com>
2023-11-12 01:12:29 -0300

    initial commit

---
+++ src/main/java/com/saturno/projeto/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.saturno.projeto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.saturno.projeto.dto.IndexDto;
+
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String indexGetRoute(Model model) {
+
+        IndexDto indexDto = new IndexDto();
+
+        model.addAttribute("indexDto", indexDto);
+
+        return "main/index";
+    }
+
+    @PostMapping
+    public String indexPostRoute(@ModelAttribute IndexDto indexDto, Model model) {
+        model.addAttribute("indexDto", indexDto);
+        return "main/result";
+    }
+}
---
+++ src/main/java/com/saturno/projeto/dto/IndexDto.java
@@ -0,0 +1,14 @@
+package com.saturno.projeto.dto;
+
+public class IndexDto {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
